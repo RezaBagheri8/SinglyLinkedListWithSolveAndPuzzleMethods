c915c0aff9098b735052ca5ed24aa1027df38e64
Reza Bagheri <bagherbagherreza@gmail.com>
2022-08-24 19:47:06 +0430

    Initial Repo

---
+++ SinglyLinkedListWithSolve-PuzzleMethods/SinglyLinkedListWithSolve-PuzzleMethods/LinkedList.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SinglyLinkedListWithSolve_PuzzleMethods
+{
+    public class LinkedList
+    {
+        public Node head;
+
+        public void InsertNodeAtFirst(int value)
+        {
+            if (head == null)
+            {
+                Node newItem = new Node();
+                newItem.value = value;
+
+                head = newItem;
+            }
+            else
+            {
+                Node temp = head;
+                Node newItem = new Node();
+                newItem.value = value;
+                newItem.next = temp;
+                head = newItem;
+            }
+        }
+
+        public void InsertNodeAtLast(int value)
+        {
+            if (head == null)
+            {
+                Node newItem = new Node();
+                newItem.value = value;
+                head = newItem;
+            }
+            else
+            {
+                Node node = head;
+                while (node.next != null)
+                {
+                    node = node.next;
+                }
+
+                Node newItem = new Node();
+                newItem.value = value;
+
+                node.next = newItem;
+            }
+        }
+
+        public void Solve(Node start)
+        {
+            if (start == null)
+            {
+                return;
+            }
+
+            Console.WriteLine(start.value);
+
+            if (start.next != null)
+            {
+                Solve(start.next.next);
+            }
+
+            Console.WriteLine(start.value);
+        }
+
+        int Puzzle(Node head)
+        {
+            if (head == null)
+            {
+                return 0;
+            }
+            else
+            {
+                return 1 + Puzzle(head.next);
+            }
+        }
+
+        public void Print()
+        {
+            if (head == null)
+            {
+                Console.WriteLine("The list is empty!");
+            }
+            else
+            {
+                Node node = head;
+                while (node != null)
+                {
+                    Console.WriteLine(node.value);
+                    node = node.next;
+                }
+            }
+        }
+
+        public void PrintSolveMethod()
+        {
+            Solve(head);
+        }
+
+        public void PrintPuzzleMethod()
+        {
+            Console.WriteLine(Puzzle(head));
+        }
+    }
+}
---
+++ SinglyLinkedListWithSolve-PuzzleMethods/SinglyLinkedListWithSolve-PuzzleMethods/Program.cs
@@ -0,0 +1,24 @@
+﻿using SinglyLinkedListWithSolve_PuzzleMethods;
+
+var list = new LinkedList();
+
+list.Print();
+list.InsertNodeAtLast(1);
+list.InsertNodeAtLast(2);
+list.InsertNodeAtLast(3);
+list.InsertNodeAtLast(4);
+list.InsertNodeAtLast(5);
+list.InsertNodeAtLast(6);
+list.InsertNodeAtLast(7);
+list.InsertNodeAtLast(8);
+list.InsertNodeAtLast(9);
+
+list.Print();
+
+
+Console.WriteLine("Puzzle Method");
+list.PrintPuzzleMethod();
+
+Console.WriteLine("Solve Method");
+list.PrintSolveMethod();
+
